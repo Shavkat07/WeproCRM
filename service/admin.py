94f0c1954c76146649557d43cc03fefecd3ac58e
Shavkat <shavkatkurbanov065@gmail.com>
2023-10-01 19:34:01 +0500

    yana bir kommit

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from service.models import Category, Service
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+    search_fields = ['name', 'description']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'stack', 'category']
+    search_fields = ['stack', 'name', 'category__name']
+
--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,10 @@
-
 from rest_framework.routers import SimpleRouter
 
 from .views import ServiceViewSet, CategoryViewSet
 
 router = SimpleRouter()
-router.register('service', ServiceViewSet)
 router.register('categories', CategoryViewSet)
+router.register('service', ServiceViewSet)
 
 urlpatterns = [
 
