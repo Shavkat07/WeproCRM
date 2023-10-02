e01455aeb3b4dbca4ce840b5c567da842c4fef45
Shavkat <shavkatkurbanov065@gmail.com>
2023-10-02 20:45:54 +0500

    DjRestAuth qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -58,8 +58,9 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEDAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
     ]
 }
 
--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,8 @@
-from dj_rest_auth.views import LogoutView
 from django.urls import path
-from rest_framework.routers import SimpleRouter
-from .views import CustomUserViewSet, LoginViewSet, PasswordChangeViewSet, PasswordResetViewSet
 
-router = SimpleRouter()
-
-router.register('signup', CustomUserViewSet)
-router.register('login', LoginViewSet, basename='custom-login')
-router.register('password/change', PasswordChangeViewSet, basename='custom-password-change')
-router.register('password/reset', PasswordResetViewSet, basename='custom-password-reset')
-# router.register('logout', LogoutView.as_view(), basename='logout')
+from users.views import SignUpView
 
 urlpatterns = [
+    path('register', SignUpView.as_view()),
 
 ]
